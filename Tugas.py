13edbaa210b137f836da7bd4194cf66d3363ec10
Azzikra Praqasta Kusuma <133850511+AzzikraPraqastaKusuma123@users.noreply.github.com>
2024-04-27 11:15:13 +0700

    Pemrograman Lanjut Tugas1 Python

--- Tugas.py
+++ Tugas.py
@@ -1,43 +1,39 @@
-def hitung_nilai_akhir(nilai_tugas, nilai_uts, nilai_uas):
-    nilai_akhir = (0.25 * nilai_tugas) + (0.35 * nilai_uts) + (0.40 * nilai_uas)
+def hitung_nilai_akhir(tugas, uts, uas):
+    nilai_akhir = 0.25 * tugas + 0.35 * uts + 0.4 * uas
     return nilai_akhir
 
-def konversi_nilai(nilai_akhir):
+def tentukan_nilai_huruf(nilai_akhir):
     if nilai_akhir > 85:
-        return "A"
-    elif 80 <= nilai_akhir <= 85:
-        return "A-"
-    elif 75 <= nilai_akhir < 80:
-        return "B+"
-    elif 70 <= nilai_akhir < 75:
-        return "B"
-    elif 65 <= nilai_akhir < 70:
-        return "B-"
-    elif 60 <= nilai_akhir < 65:
-        return "C+"
-    elif 55 <= nilai_akhir < 60:
-        return "C"
-    elif 50 <= nilai_akhir < 55:
-        return "C-"
-    elif 30 <= nilai_akhir < 50:
-        return "D"
+        return 'A'
+    elif nilai_akhir > 80:
+        return 'A-'
+    elif nilai_akhir > 75:
+        return 'B+'
+    elif nilai_akhir > 70:
+        return 'B'
+    elif nilai_akhir > 65:
+        return 'B-'
+    elif nilai_akhir > 60:
+        return 'C+'
+    elif nilai_akhir > 55:
+        return 'C'
+    elif nilai_akhir > 50:
+        return 'C-'
+    elif nilai_akhir > 30:
+        return 'D'
     else:
-        return "E"
+        return 'E'
 
-print("Selamat datang di Aplikasi perhitungan nilai Mahasiswa")
+def main():
+    tugas = float(input("Masukkan nilai tugas: "))
+    uts = float(input("Masukkan nilai UTS: "))
+    uas = float(input("Masukkan nilai UAS: "))
 
-# Nilai tugas, UTS, dan UAS sudah ditentukan sebelumnya
-nilai_tugas = 89
-nilai_uts = 90
-nilai_uas = 90
+    nilai_akhir = hitung_nilai_akhir(tugas, uts, uas)
+    nilai_huruf = tentukan_nilai_huruf(nilai_akhir)
 
-# Hitung nilai akhir
-nilai_akhir = hitung_nilai_akhir(nilai_tugas, nilai_uts, nilai_uas)
+    print("Nilai akhir:", nilai_akhir)
+    print("Nilai huruf:", nilai_huruf)
 
-# Konversi nilai akhir 
-nilai_huruf = konversi_nilai(nilai_akhir)
-# Output hasil
-print(f"Nilai Tugas Anda: {nilai_tugas}")
-print(f"Nilai UTS Anda: {nilai_uts}")
-print(f"Nilai UAS Anda: {nilai_uas}")
-print(f"Nilai Akhir Anda: {nilai_akhir},\nDalam Huruf {nilai_huruf}")
+if __name__ == "__main__":
+    main()
